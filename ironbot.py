1889465cad63034b83150445291965fe63dbf407
Zerrossetto <tamaghei@gmail.com>
2016-10-23 18:16:11 +0200

    Added scheduler and autorestore of set-server-start time

--- ironbot.py
+++ ironbot.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging.config
+import schedule
 import settings
 import traceback
 from commons import messages, errors
@@ -7,6 +8,14 @@ from discord import DiscordException
 from discord.ext.commands import Bot, Context
 
 log = logging.getLogger(settings.LOGGER_IRONBOT)
+
+
+@asyncio.coroutine
+def scheduler_tick(ironbot: Bot):
+    yield from ironbot.wait_until_ready()
+    while not ironbot.is_closed:
+        schedule.run_pending()
+        yield from asyncio.sleep(1)
 
 
 def main():
@@ -17,7 +26,9 @@ def main():
     log.debug('Retrieved i18n dictionaries')
     log.debug(messages.msg_map)
     log.info('i18n tables loaded. Available languages are [{}]'.format(', '.join(messages.msg_map.keys())))
+
     ironbot = Bot(**settings.BOT)
+
     for ext in ('cogs.basic', 'cogs.maple'):
         try:
             ironbot.load_extension(ext)
@@ -28,12 +39,20 @@ def main():
     @ironbot.event
     @asyncio.coroutine
     def on_ready():
+
         log.info('Logged in as {} id={}'.format(ironbot.user.name, ironbot.user.id))
+
+        log.info('Starting global scheduler')
+        log.debug('Scheduled jobs are: ')
+
+        for job in schedule.jobs:
+            log.debug(job)
 
     @ironbot.event
     @asyncio.coroutine
     def on_error(event):
         log.error('An error occurred: {}'.format(event))
+        log.error(traceback.extract_tb())
 
     @ironbot.event
     @asyncio.coroutine
@@ -45,7 +64,9 @@ def main():
         else:
             log.warn('Unhandled command error: {}'.format(error))
 
+    ironbot.loop.create_task(scheduler_tick(ironbot))
     ironbot.run(settings.DISCORD_TOKEN)
+
 
 if __name__ == '__main__':
     main()
