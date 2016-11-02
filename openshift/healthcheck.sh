1475ceb52f3674f4774d8f56d03c783bc44688de
Zerrossetto <tamaghei@gmail.com>
2016-11-02 23:12:13 +0100

    healthcheck improvement

--- openshift/healthcheck.sh
+++ openshift/healthcheck.sh
@@ -2,23 +2,25 @@
 
 if [[ -f ${HEARTBEAT_FILE} ]]; then
 
+    # getting last modified date from the hearbeat file
     heartbeat=$(stat -c %Y ${HEARTBEAT_FILE} 2>/dev/null)
     if [ -z "$heartbeat" ]; then
         # fix for BSD systems
         heartbeat=$(stat -f %m ${HEARTBEAT_FILE} 2>/dev/null)
     fi
+
     if [ -z "$heartbeat" ]; then
         logger -s "Unable to retrieve last heartbeat time"
-        exit 2
-    fi
-
-    if [[ $(( $(date +%s) - heartbeat )) > $1 ]]; then
+        exit_code=2
+    elif [[ $(( $(date +%s) - heartbeat )) > $1 ]]; then
         logger -s "Heartbeat token expired (current limit is $1 seconds)"
-        exit 1
+        exit_code=1
+    else
+        exit_code=0
     fi
 else
     logger -s "Heartbeat file \"$HEARTBEAT_FILE\" not found"
-    exit 1
+    exit_code=1
 fi
 
-exit 0
+exit ${exit_code}
