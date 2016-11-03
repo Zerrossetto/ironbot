f944353b8950f99815bf24579b490c98ad39fbf4
Zerrossetto <tamaghei@gmail.com>
2016-11-03 19:35:29 +0100

    Moved script to sh from bash

--- openshift/healthcheck.sh
+++ openshift/healthcheck.sh
@@ -1,25 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-if [[ -f ${HEARTBEAT_FILE} ]]; then
+file_name=${HEARTBEAT_FILE-'/tmp/ironbot-heartbeat'}
+
+if [[ -f ${file_name} ]]; then
 
     # getting last modified date from the hearbeat file
-    heartbeat=$(stat -c %Y ${HEARTBEAT_FILE} 2>/dev/null)
+    heartbeat=$(stat -c %Y ${file_name} 2>/dev/null)
     if [ -z "$heartbeat" ]; then
         # fix for BSD systems
-        heartbeat=$(stat -f %m ${HEARTBEAT_FILE} 2>/dev/null)
+        heartbeat=$(stat -f %m ${file_name} 2>/dev/null)
     fi
 
     if [ -z "$heartbeat" ]; then
         logger -s "Unable to retrieve last heartbeat time"
         exit_code=2
-    elif [[ $(( $(date +%s) - heartbeat )) > $1 ]]; then
-        logger -s "Heartbeat token expired (current limit is $1 seconds)"
-        exit_code=1
     else
-        exit_code=0
+        limit=${1-600}
+        diff=$(( $(date +%s) - heartbeat ))
+        if [ "$diff" -gt "$limit" ]; then
+            logger -s "Heartbeat token expired (with $diff > $limit seconds)"
+            exit_code=1
+        else
+            # Everything's fine
+            exit_code=0
+        fi
     fi
 else
-    logger -s "Heartbeat file \"$HEARTBEAT_FILE\" not found"
+    logger -s "Heartbeat file \"$file_name\" not found"
     exit_code=1
 fi
 
